2ed656fd88bda5a74e6e9634a61f37a0ef78b9e9
Eric Nelson <enelson@zendesk.com>
2022-05-20 10:01:39 -0400

    Comment up code

--- hyperlinkFinder.js
+++ hyperlinkFinder.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 var axios = require("axios");
 
+// Encode the authentication credentials
 const encodedToken = Buffer.from(
   `${process.env.ZD_USER}/token:${process.env.ZD_TOKEN}`
 ).toString("base64");
@@ -16,8 +17,10 @@ let config = {
 
     axios(config)
     .then(function (response) {
+    // Call the api and return the articles array
       let sourceArr = response.data.articles
       let hyperLinkArr = []
+      //Loop through article array and push any that have an <a> tag into a new array and return to the console
       for(let i = 0; i < sourceArr.length; i++){
         let body = sourceArr[i].body
         if (body.indexOf("<a href=")!= -1){
